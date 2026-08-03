9e23c862031083fce3ac0cb0a705a446a1ca8860
Gabriel Takahata <gabrieltakahata.dev@gmail.com>
2026-08-03 19:47:47 -0300

    otimiza código

---
+++ script.js
@@ -0,0 +1,9 @@
+// Requisitos funcionais(definem o que o sistema deve fazer (ações e regras de negócio) e não funcionais do sistema determinam como o sistema deve operar (qualidade, velocidade e segurança);
+
+// ALGORITMO: conjunto de funções (sequência de passos, ordenada para executar uma ação em específico)
+
+// Capturar as informações das caixas de texto e mandar para o Backend;
+
+let formInput = document.getElementById("form-input");
+let formInputTwo = document.getElementById("form-input-two");
+let mainTitle = document.getElementById("left-title");
